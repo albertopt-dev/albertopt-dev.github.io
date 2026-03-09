93fce22bda5fd9cf9274240657be4e4cec468ba0
albertopt-dev <alberto.pt.dev@gmail.com>
2026-03-09 17:46:08 +0100

    inicialización del proyecto portfolio-web

---
+++ app.js
@@ -0,0 +1,11 @@
+// Proyectos 
+const projects = [
+    {
+        title: "Portfolio Web",
+        description: "Mi portfolio personal, construido con HTML, CSS y JavaScript. Muestra mis habilidades, proyectos y experiencia de manera clara y atractiva.",
+        tags: ["Web", "Frontend"],
+        stacks: ["HTML", "CSS", "JavaScript"],
+        links: {
+            repo: "https://github.com/albertopt-dev/Portfolio-Web.git"
+        }
+    }
